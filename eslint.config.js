0ce0656e3cd5dd89b736439c2083ae45e757c3b7
BrunoGino <bruno.gino24@gmail.com>
2025-06-25 18:17:53 +0000

    chore: adjusted eslint and libraries

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,26 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-
+import prettierplugin from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import { includeIgnoreFile } from '@eslint/compat';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  eslintConfigPrettier
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  includeIgnoreFile(
+    fileURLToPath(new URL('.gitignore', import.meta.url)),
+    'Imported .gitignore patterns'
+  ),
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylistic,
+  {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        projectService: {
+          allowDefaultProject: ['.prettierrc.js', '*.js'],
+        },
+      },
+    },
+  },
+  prettierplugin,
 ]);
